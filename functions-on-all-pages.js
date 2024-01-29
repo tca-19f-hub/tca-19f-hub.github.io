e6419bc6c35880bb0dd2ac1bd3df9340d668a85b
tca-19f-hub <133700669+tca-19f-hub@users.noreply.github.com>
2024-01-29 23:00:51 +0300

    back to top

---
+++ functions-on-all-pages.js
@@ -0,0 +1,5 @@
+// Скролл вверх страницы - в начало
+function topFunction() {
+    document.body.scrollTop = 0; // для Safari
+    document.documentElement.scrollTop = 0; // для Chrome, Firefox, IE and Opera
+  }
