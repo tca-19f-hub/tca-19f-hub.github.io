d3045e547f78c749832f50b29fcfbbabee6a769d
Prince-OFlynn <133700669+tca-19f-hub@users.noreply.github.com>
2024-04-11 00:37:45 +0300

    test

--- function-oblivion.js
+++ function-oblivion.js
@@ -4,10 +4,9 @@ function collapse_1() {
 }
 
 function play_sound_1() {
-    var audio_1 = new Audio({
-        loop: true,
-        volume: 0.2,
-        src: ['https://audio.jukehost.co.uk/qyJVTdlOfAJok4s5gHh62yNpsgjmVvgE']
-    })
+    var audio_1 = new Audio('https://audio.jukehost.co.uk/qyJVTdlOfAJok4s5gHh62yNpsgjmVvgE')
     audio_1.play();
+    audio_1.volume = 0.2;
+    audio_1.loop = true;
+
 }
