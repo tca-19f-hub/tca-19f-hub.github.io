9cae7cb2b7c536d07fc218fb0a51a2c7a64631ac
Prince-OFlynn <133700669+tca-19f-hub@users.noreply.github.com>
2024-05-05 00:58:58 +0300

    fuck me

--- function-oblivion.js
+++ function-oblivion.js
@@ -2,11 +2,23 @@ function collapse_1() {
     document.getElementById("collapse-1").style.display = "block";
     document.getElementById("button-1").style.display = "none";
 }
-
 function play_sound_1() {
     var audio_1 = new Audio('https://audio.jukehost.co.uk/qyJVTdlOfAJok4s5gHh62yNpsgjmVvgE')
     audio_1.play();
     audio_1.volume = 0.02;
     audio_1.loop = true;
+
+}
+function collapse_2() {
+    document.getElementById("collapse-2").style.display = "block";
+    document.getElementById("button-2").style.display = "none";
+}
+function play_sound_2() {
+    var audio_2 = new Audio('')
+    audio_2.play();
+    audio_2.volume = 0.02;
+    audio_2.loop = true;
+    audio_1.pause();
+    audio_1.currentTime = 0;
 
 }
