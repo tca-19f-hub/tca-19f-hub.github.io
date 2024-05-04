d3ef65602df0be5e0eaed1c376868482ef8caf58
Prince-OFlynn <133700669+tca-19f-hub@users.noreply.github.com>
2024-05-05 01:24:45 +0300

    fuckme

--- function-oblivion.js
+++ function-oblivion.js
@@ -1,5 +1,14 @@
 var audio_1 = new Audio('https://audio.jukehost.co.uk/qyJVTdlOfAJok4s5gHh62yNpsgjmVvgE')
 var audio_2 = new Audio('')
+var audio_3 = new Audio('')
+var audio_4 = new Audio('')
+var audio_5 = new Audio('')
+var audio_6 = new Audio('')
+var audio_7 = new Audio('')
+var audio_8 = new Audio('')
+var audio_9 = new Audio('')
+var audio_10 = new Audio('')
+
 
 function collapse_1() {
     document.getElementById("collapse-1").style.display = "block";
@@ -9,8 +18,8 @@ function play_sound_1() {
     audio_1.play();
     audio_1.volume = 0.02;
     audio_1.loop = true;
-
 }
+
 function collapse_2() {
     document.getElementById("collapse-2").style.display = "block";
     document.getElementById("button-2").style.display = "none";
@@ -21,5 +30,112 @@ function play_sound_2() {
     audio_2.loop = true;
     audio_1.pause();
     audio_1.currentTime = 0;
+}
 
+function collapse_3() {
+    document.getElementById("collapse-3").style.display = "block";
+    document.getElementById("button-3").style.display = "none";
+}
+function play_sound_3() {
+    audio_3.play();
+    audio_3.volume = 0.02;
+    audio_3.loop = true;
+    audio_2.pause();
+    audio_2.currentTime = 0;
+}
+
+function collapse_4() {
+    document.getElementById("collapse-4").style.display = "block";
+    document.getElementById("button-4").style.display = "none";
+}
+function play_sound_4() {
+    audio_4.play();
+    audio_4.volume = 0.02;
+    audio_4.loop = true;
+    audio_3.pause();
+    audio_3.currentTime = 0;
+}
+
+function collapse_5() {
+    document.getElementById("collapse-5").style.display = "block";
+    document.getElementById("button-5").style.display = "none";
+}
+function play_sound_5() {
+    audio_5.play();
+    audio_5.volume = 0.02;
+    audio_5.loop = true;
+    audio_4.pause();
+    audio_4.currentTime = 0;
+}
+
+function collapse_6() {
+    document.getElementById("collapse-6").style.display = "block";
+    document.getElementById("button-6").style.display = "none";
+}
+function play_sound_6() {
+    audio_6.play();
+    audio_6.volume = 0.02;
+    audio_6.loop = true;
+    audio_5.pause();
+    audio_5.currentTime = 0;
+}
+
+function collapse_7() {
+    document.getElementById("collapse-7").style.display = "block";
+    document.getElementById("button-7").style.display = "none";
+}
+function play_sound_7() {
+    audio_7.play();
+    audio_7.volume = 0.02;
+    audio_7.loop = true;
+    audio_6.pause();
+    audio_6.currentTime = 0;
+}
+
+function collapse_8() {
+    document.getElementById("collapse-8").style.display = "block";
+    document.getElementById("button-8").style.display = "none";
+}
+function play_sound_8() {
+    audio_8.play();
+    audio_8.volume = 0.02;
+    audio_8.loop = true;
+    audio_7.pause();
+    audio_7.currentTime = 0;
+}
+
+function collapse_9() {
+    document.getElementById("collapse-9").style.display = "block";
+    document.getElementById("button-9").style.display = "none";
+}
+function play_sound_9() {
+    audio_9.play();
+    audio_9.volume = 0.02;
+    audio_9.loop = true;
+    audio_8.pause();
+    audio_8.currentTime = 0;
+}
+
+function collapse_10() {
+    document.getElementById("collapse-10").style.display = "block";
+    document.getElementById("button-10").style.display = "none";
+}
+function play_sound_10() {
+    audio_10.play();
+    audio_10.volume = 0.02;
+    audio_10.loop = true;
+    audio_9.pause();
+    audio_9.currentTime = 0;
+}
+
+function collapse_11() {
+    document.getElementById("collapse-11").style.display = "block";
+    document.getElementById("button-11").style.display = "none";
+}
+function play_sound_11() {
+    audio_11.play();
+    audio_11.volume = 0.02;
+    audio_11.loop = true;
+    audio_10.pause();
+    audio_10.currentTime = 0;
 }
