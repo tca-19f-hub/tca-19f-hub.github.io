bef6fac713c5240a3df32f9b2c09d5a1c33b992f
Prince-OFlynn <133700669+tca-19f-hub@users.noreply.github.com>
2024-05-10 22:32:29 +0300

    rbslhfgef

--- function-i-was-happy-to-see-you.js
+++ function-i-was-happy-to-see-you.js
@@ -8,7 +8,27 @@ document.addEventListener('mousemove', (e) => {
      
     mouseFollow.style.top = `${y}px`;
     mouseFollow.style.left = `${x}px`;
-  })
+})
+document.addEventListener('mousemove', (e) => {
+
+    const mouseFollow = document.getElementById('c-2');
+    const x = e.clientX - 25; //-25 to center div over mouse
+    const y = e.clientY - 25; 
+    console.log(x);
+     
+    mouseFollow.style.top = `${y}px`;
+    mouseFollow.style.left = `${x}px`;
+})
+document.addEventListener('mousemove', (e) => {
+
+    const mouseFollow = document.getElementById('c-3');
+    const x = e.clientX - 25; //-25 to center div over mouse
+    const y = e.clientY - 25; 
+    console.log(x);
+     
+    mouseFollow.style.top = `${y}px`;
+    mouseFollow.style.left = `${x}px`;
+})
 
 // Подсказка появляется и исчезает
 function show_1() {
@@ -17,12 +37,14 @@ function show_1() {
 function hide_1() {
     document.getElementById('c-1').style.display = 'none';
 }
+
 function show_2() {
     document.getElementById('c-2').style.display = 'block';
 }
 function hide_2() {
     document.getElementById('c-2').style.display = 'none';
 }
+
 function show_3() {
     document.getElementById('c-3').style.display = 'block';
 }
