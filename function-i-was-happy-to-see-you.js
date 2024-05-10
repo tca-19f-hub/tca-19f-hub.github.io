ec161ac81a6359f8ef7f93427ddbf688af8dc507
Prince-OFlynn <133700669+tca-19f-hub@users.noreply.github.com>
2024-05-10 21:58:45 +0300

    бля

--- function-i-was-happy-to-see-you.js
+++ function-i-was-happy-to-see-you.js
@@ -11,11 +11,6 @@ document.addEventListener('mousemove', (e) => {
   })
 
 // Подсказка появляется и исчезает
-window.onmousemove = function() {
-    var div = document.querySelector('#c-1');
-    div.style.display = document.querySelectorAll('#t-1:hover').length ? 'none' : 'block';
-}
-
 var raz1 = document.getElementById('t-1');
 raz1.onmouseover = function() {
   document.getElementById('c-1').style.display = 'block';
