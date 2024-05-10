8e8efdb2d4291ab95c1d4f5e8ffcad97fe8f736d
Prince-OFlynn <133700669+tca-19f-hub@users.noreply.github.com>
2024-05-10 15:26:46 +0300

    new

--- functions-on-all-pages.js
+++ functions-on-all-pages.js
@@ -3,3 +3,15 @@ function topFunction() {
     document.body.scrollTop = 0; // для Safari
     document.documentElement.scrollTop = 0; // для Chrome, Firefox, IE and Opera
   }
+
+// Элемент следует за курсором
+const move = document.getElementById("move");
+document.body.onpointermove = event => {
+  const { clientX, clientY } = event;
+  
+  move.animate({
+    left: `${clientX}px`,
+    top: `${clientY}px`
+      
+  }, {duration: 1000, fill: "forwards"})
+}
